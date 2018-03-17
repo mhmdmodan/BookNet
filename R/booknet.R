b0762306429cb4edd04f1ea2996ade77b8ff5bdf
mhmdmodan <mhmd.modan@gmail.com>
2018-03-16 19:10:47 -0500

    documentation, package complete

--- R/booknet.R
+++ R/booknet.R
@@ -1,3 +1,30 @@
+#' BookNet
+#'
+#' Reads a directory of text files and generates
+#' an adjacency matrix of characters in those text files, with weights
+#' equal to the number of times the relationship appeared.
+#' A connection is made when characters appear within
+#' window_length tokens of each other, delimited by spaces.
+#' The directory must contain one "names.txt" file which
+#' consists of Java regular expressions one each line to search for, for a
+#' given character, ie "Jaime|Kingslayer" would match for
+#' Jaime Lannister in ASOIAF.
+#'
+#' @param folder_path path to a directory which contains the *.txt files (including "names.txt")
+#' @param vertex_names optional - names for each row/column in the matrix, and vertex labels for `igraph`.
+#' Must be specified in the same order as in names.txt. If missing labels from names.txt will be used instead.
+#' @param window_length window of words to check for character occurences.
+#' @param min_occurences weights in the matrix below this number will be set to zero.
+#' @param percent_remove removes the bottom `percent_remove` percentage of nonzero weights.
+#' If `min_occurences` is specified, those occurences will be removed first, before removing
+#' the bottom percentage specified by `percent_remove`.
+#'
+#' @return a list, with `$adjacency_matrix` returning the labeled adjacency matrix,
+#' and `$adj_graph` returning an `igraph` graph.
+#' @export
+#'
+#' @examples
+#' out <- booknet("data/ASOIAF_Folder", min_occurences = 3)
 booknet <- function(folder_path, vertex_names, window_length = 15, min_occurences = 0, percent_remove = 0) {
   class_name <- J("BookNet")
   full_path <- gsub("\\\\", "//",normalizePath(folder_path))
